2d0398924de9c2ca9e8e00b257924f9b609c6d8a
ruf <ruf@knowit.no>
2016-10-21 17:47:59 +0200

    Hidden secret word

--- src/Main.elm
+++ src/Main.elm
@@ -3,12 +3,13 @@ module Main exposing (..)
 import Html.App as App
 import Views exposing (..)
 import State exposing (..)
+import SecretWord
 
 
 main : Program Never
 main =
     App.beginnerProgram
-        { model = newStandardGame "typesafefrontend"
+        { model = newStandardGame SecretWord.secretWord
         , view = view
         , update = update
         }
