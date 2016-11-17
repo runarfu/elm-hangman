b5a4a542e153410c44cc1583ce8ac2631fa9567f
Jordy Moos <jordy@pbwebmedia.com>
2016-11-17 17:39:27 +0100

    Update for elm 0.18

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,16 @@
 module Main exposing (..)
 
-import Html.App as App
+import Html exposing (beginnerProgram)
+import Types exposing (Game, Msg)
 import GameUtils
 import Secrets
 import Views
 import State
 
 
-main : Program Never
+main : Program Never Game Msg
 main =
-    App.beginnerProgram
+    Html.beginnerProgram
         { model = GameUtils.newStandardGame Secrets.secretWords
         , view = Views.view
         , update = State.update
